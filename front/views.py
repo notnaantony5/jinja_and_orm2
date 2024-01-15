79e3529a3f816926a3414a615bc8bc8a336ee80e
Admin <ewgfwgf@geswfrg.ru>
2024-01-15 21:03:46 +0300

    init

--- front/views.py
+++ front/views.py
@@ -26,4 +26,5 @@ def index(request):
     ]
     return render(request,
                   'index.html',
-                  {'title': 'Главная'})
+                  {'title': 'Главная',
+                   'posts': posts})
